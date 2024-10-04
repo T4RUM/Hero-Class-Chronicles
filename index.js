08bcf0f93a6586d2b9e645f3f3a746ca8a127b30
T4RUM <wesley-murat@hotmail.com>
2024-10-04 15:57:57 -0300

    Resolução do desafio

---
+++ index.js
@@ -0,0 +1,36 @@
+class Heroi {
+  constructor(nome, idade, tipo) {
+      this.nome = nome;      
+      this.idade = idade;   
+      this.tipo = tipo;      
+  }
+
+  atacar() {
+      let ataque;
+      
+      if (this.tipo === 'mago') {
+          ataque = 'magia';
+      } else if (this.tipo === 'guerreiro') {
+          ataque = 'espada';
+      } else if (this.tipo === 'monge') {
+          ataque = 'artes marciais';
+      } else if (this.tipo === 'ninja') {
+          ataque = 'shuriken';
+      } else {
+          ataque = 'ataque desconhecido';
+      }
+
+      // 4. Exibir a mensagem de ataque no formato desejado
+      console.log(`${this.tipo} atacou usando ${ataque}`);
+  }
+}
+
+const heroi1 = new Heroi('Gandalf', 1000, 'mago');
+const heroi2 = new Heroi('Aphonse', 87, 'guerreiro');
+const heroi3 = new Heroi('Liu', 45, 'monge');
+const heroi4 = new Heroi('Naruto', 20, 'ninja');
+
+heroi1.atacar(); 
+heroi2.atacar(); 
+heroi3.atacar(); 
+heroi4.atacar(); 
